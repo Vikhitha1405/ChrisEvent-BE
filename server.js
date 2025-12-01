0c37ff697c14486421dbd3d21b33bf6768dfe889
Vikhitha1405 <vikhithav68@gmail.com>
2025-12-01 23:14:26 +0530

    second commit

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json()); // IMPORTANT: allows reading req.body
-/
+
 // MySQL connection
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "parro_event"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
+
 
 // Insert login details into DB
 app.post('/login', (req, res) => {
